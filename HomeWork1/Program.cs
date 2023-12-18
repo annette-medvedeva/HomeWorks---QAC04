8a15099c3c2a306c78557960ef9a809951845ef5
Anna Medvedeva <anna@eunify.net>
2023-12-18 17:30:10 +0200

    Create first programm

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -12,12 +12,14 @@ namespace HomeWork1
         {
             string name;
             string lastName;
+            int age;
             Console.WriteLine("First Programm");
             Console.WriteLine("Enter your First Name: ");
             name=Console.ReadLine();
             Console.WriteLine("Enter your Second Name: ");
             lastName=Console.ReadLine();
-            Console.WriteLine($"Hello, {name} {lastName}");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hello, {name} {lastName}, you are {age} years old.");
             Console.ReadLine();
         }
     }
