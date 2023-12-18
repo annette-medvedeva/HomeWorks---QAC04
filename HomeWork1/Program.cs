7db6d44f7be23a95fc16a8f4c24ebd48c84bb76c
Anna Medvedeva <anna@eunify.net>
2023-12-18 17:02:54 +0200

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string lastName;
+            Console.WriteLine("First Programm");
+            Console.WriteLine("Enter your First Name: ");
+            name=Console.ReadLine();
+            Console.WriteLine("Enter your Second Name: ");
+            lastName=Console.ReadLine();
+            Console.WriteLine($"Hello, {name} {lastName}");
+            Console.ReadLine();
+        }
+    }
+}
